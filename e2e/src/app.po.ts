fd2b12e9cc1290b500c81f2e7b2752b7031781ac
BurekLord <mniole@yahoo.com>
2019-10-30 22:42:16 +0100

    added homeComponent route

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -6,6 +6,6 @@ export class AppPage {
     }
 
     getTitleText() {
-        return element(by.css('do-root h1')).getText() as Promise<string>;
+        return element(by.css('do-home p')).getText() as Promise<string>;
     }
 }
