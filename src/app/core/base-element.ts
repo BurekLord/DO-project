5a29ad86182cc2603446c9cb87945add9149d843
BurekLord <mniole@yahoo.com>
2019-11-30 11:08:23 +0100

    size regex update

--- src/app/core/base-element.ts
+++ src/app/core/base-element.ts
@@ -3,8 +3,13 @@ import { getClassFromSize } from './utils';
 
 export class BaseElement {
     @Input() set size(value: string) {
+        value = '12|12|1fd'
         if (value) {
-            this.sizeClass = getClassFromSize(value);
+            console.log('regex', value.match(this.regExp));
+            if (value.match(this.regExp)) {
+                console.log('regex2', value.match(this.regExp));
+                this.sizeClass = getClassFromSize(value);
+            }
         }
     }
     @Input() set subElementSize(value: string) {
@@ -17,6 +22,9 @@ export class BaseElement {
             this.labelSizeClass = getClassFromSize(value);
         }
     }
+
+    regExp = /([1-9][1-9]\|[1-9][1-9]\|[1-9][1-9])|([1-9][1-9]\|[1-9][1-9]\||[1-9][1-9]\|[1-9][1-9])/;
+
     sizeClass: string;
     subElementSizeClass: string;
     labelSizeClass: string;
