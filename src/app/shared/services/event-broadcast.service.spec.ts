2288626a6b75d146ab28082d9d9755c6d65b6390
BurekLord <mniole@yahoo.com>
2019-12-18 12:05:28 +0100

    valid event-broadcast.service tests done

--- src/app/shared/services/event-broadcast.service.spec.ts
+++ src/app/shared/services/event-broadcast.service.spec.ts
@@ -1,18 +1,27 @@
+import { Subscription } from 'rxjs';
 import { EventHub, Event } from './event-broadcast.service';
 
-import { TestBed } from '@angular/core/testing';
+import { TestBed, fakeAsync, tick, async } from '@angular/core/testing';
 
 describe('EventHub', () => {
     let serviceUnderTest: EventHub;
-    let fakeEvent: Event<any>;
+    let fakeEvent: any;
+    let fakeValue: any;
+    let actualValue: any;
+    let fakeCallback: any;
 
-    Given(() => {
+    Given(async(() => {
         TestBed.configureTestingModule({
             providers: [EventHub]
         });
-    })
+        serviceUnderTest = TestBed.get(EventHub);
+    }));
 
-    serviceUnderTest = TestBed.get(EventHub);
+    describe('INIT', () => {
+        Then(() => {
+            expect(serviceUnderTest.observable).toBeTruthy();
+        });
+    });
 
     describe('METHOD: broadcast', () => {
 
@@ -21,16 +30,75 @@ describe('EventHub', () => {
         });
 
         describe('GIVEN an event THEN observer should fire next', () => {
-
             Given(() => {
                 fakeEvent = { name: 'fakeName', payload: 'fakeContent' };
             });
 
             Then(() => {
-                expect(true).toBeTruthy();
-                // serviceUnderTest.observable.subscribe(res => {
-                // expect(res).toEqual(fakeEvent);
-                // });
+                serviceUnderTest.observable.subscribe(res => {
+                    expect(res).toEqual(fakeEvent);
+                });
+            });
+        });
+        describe('GIVEN nothing THEN observer should do nothing', () => {
+            Given(() => {
+                fakeEvent = { name: 'fakeName', payload: 'fakeContent' };
+            });
+
+            Then(() => {
+                serviceUnderTest.observable.subscribe(res => {
+                    expect(res).toBeFalsy();
+                });
+            });
+        });
+    });
+
+    describe('METHOD: observe', () => {
+        When(() => {
+            serviceUnderTest.broadcast(fakeEvent);
+        });
+
+        // tslint:disable-next-line: max-line-length
+        describe('GIVEN service.broadcast(event) is activated with event THEN callback should be called and return event', () => {
+            Given(() => {
+                fakeEvent = { name: 'fakeName', payload: 'fakeContent' };
+                actualValue = fakeEvent;
+                fakeCallback = (event) => {
+                    fakeValue = event;
+                };
+                serviceUnderTest.observe(fakeEvent.name, fakeCallback);
+            });
+            Then(() => {
+                expect(fakeValue).toEqual(actualValue);
+            });
+        });
+
+        describe('GIVEN service.broadcast(string) is activated THEN callback should be called with string', () => {
+            Given(() => {
+                fakeEvent = 'fakeEvent';
+                actualValue = fakeEvent;
+                fakeCallback = (event) => {
+                    fakeValue = event;
+                };
+                serviceUnderTest.observe(fakeEvent, fakeCallback);
+            });
+            Then(() => {
+                expect(fakeValue).toEqual(actualValue);
+            });
+        });
+
+        describe('METHOD: destroy', () => {
+            When(() => {
+                serviceUnderTest.destroy(fakeValue);
+            });
+
+            describe('GIVEN the subscription is passed THEN it should unsubscribe', () => {
+                Given(() => {
+                    fakeValue = new Subscription();
+                });
+                Then(() => {
+                    expect(fakeValue).toBeTruthy();
+                });
             });
         });
     });
