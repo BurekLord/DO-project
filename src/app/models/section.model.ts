a05e3da90f865784ca2c536eb3b01e50ca7ba68c
BurekLord <mniole@yahoo.com>
2019-12-24 12:39:52 +0100

    improved section and task models

--- src/app/models/section.model.ts
+++ src/app/models/section.model.ts
@@ -1,6 +1,7 @@
 export class Section {
     constructor(
         public name: string,
+        public icon: string,
         public shown: boolean
     ) { }
 }
--- src/app/models/task.model.ts
+++ src/app/models/task.model.ts
@@ -4,27 +4,27 @@ import { Label } from './label.model';
 import { Reminder } from './reminder.model';
 export class Task {
     constructor(
-        public id: string,
-        public title: string,
-        public creator: ShortUser,
-        public assignee: ShortUser,
-        public projectId: string,
-        public createdOn: string,
-        public updatedOn: string,
-        public watchers: ShortUser[],
-        public description: string,
-        public items: Label[], // files, images etc
-        public startDate: string,
-        public endDate: string,
-        public estimatedTime: string,
-        public spentTime: string,
-        public status: Label,
-        public reminders: Reminder[],
-        public subTaskIds: (Task | Section)[], // id`s of subtasks
-        public likes: number,
-        public priority: Label,
-        public completed: boolean,
-        public availability: 'public' | 'private',
-        public customFields: any[] // TODO: not sure about this yet...
+        public id?: string,
+        public title?: string,
+        public creator?: ShortUser,
+        public assignee?: ShortUser,
+        public projectId?: string[],
+        public createdOn?: string,
+        public updatedOn?: string,
+        public watchers?: ShortUser[],
+        public description?: string,
+        public items?: Label[], // files, images etc
+        public startDate?: string,
+        public endDate?: string,
+        public estimatedTime?: string,
+        public spentTime?: string,
+        public status?: Label,
+        public reminders?: Reminder[],
+        public subTaskIds?: (Task | Section)[],
+        public likes?: number,
+        public priority?: Label,
+        public done?: boolean,
+        public availability?: 'public' | 'private',
+        public customFields?: any[] // TODO: not sure about this yet...
     ) { }
 }
